fc23db69ad0450651dc78de8e12ecdc71f34a67e
shaxmardan1998 <shaxmardan79@gmail.com>
2023-01-19 16:36:46 +0500

    Added 4 new file

---
+++ js/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          bgcolor: '#FFF2F3',
+          txtcolor: '#737373',
+          btncolor: '#96BB7C',
+          fontcolor: '#252B42',
+        }
+      }
+    }
+  }
